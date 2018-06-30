6a54f981854485677cc73dfb41fba13dfafb13e2
shivam4819 <shivam4819@gmail.com>
2018-06-30 22:06:08 +0530

    second commit

--- Mysql.java
+++ Mysql.java
@@ -3,90 +3,220 @@ package mysql;
 import java.sql.*;
 import java.util.Scanner;
 import java.util.Properties;
+
 public class Mysql {
 
-    private static final String dbClassName = "com.mysql.jdbc.Driver";
-    private static final String CONNECTION ="jdbc:mysql://127.0.0.1/student";
-    Scanner s= new Scanner(System.in);
-    private int roll,phone;
-    private String name,subject;
-   
-    public void studentinfo(){
+    public static void main(String[] args){
         
-        System.out.print("Enter roll no-");
-        roll=s.nextInt();
-        s.nextLine();
-        System.out.print("Enter name-");
-        name=s.nextLine();
-        System.out.print("Enter subject-");
-        subject=s.nextLine();
-        System.out.print("Enter phone no-");
-        phone=s.nextInt();
-    }
-    public static void main(String[] args) throws ClassNotFoundException,SQLException{
+    int roll,phone,opt;
+    String name,subject,company,client;
+    Scanner sc= new Scanner(System.in);
     
-    int choice;
-    Scanner sc = new Scanner(System.in);    
-    Mysql my = new Mysql();
+    Student s =new Student();
+    Company c =new Company();
     
-    System.out.println(dbClassName);
-    Class.forName(dbClassName);
-
-    Properties p = new Properties();
-    p.put("user","root");
-    p.put("password","");
-
-    Connection c = DriverManager.getConnection(CONNECTION,p);
-    Statement smt = c.createStatement();
     do{
-    System.out.println("menu");
-    System.out.println("1)insert");
-    System.out.println("2)display");
-    System.out.print("enter choice-");
-    choice = sc.nextInt();
-        switch(choice){
-     
-            case 1:    
-                    my.studentinfo();
-                    smt.executeUpdate("INSERT INTO `student_info`(roll , name , subject , phone)"+
-                            "VALUES('"+my.roll+"','"+my.name+"','"+my.subject+"','"+my.phone+"')");
-    
-                    System.out.println("It works !");
-                    break;
-                    
+        System.out.println("for student table-");
+        System.out.println("for company table-");
+        System.out.print("enter choice-");
+        opt=sc.nextInt();
+        
+        switch(opt){
+            case 1:
+                System.out.print("Enter roll no-");
+                roll=sc.nextInt();
+                sc.nextLine();
+                System.out.print("Enter name-");
+                name=sc.nextLine();
+                System.out.print("Enter subject-");
+                subject=sc.nextLine();
+                System.out.print("Enter phone no-");
+                phone=sc.nextInt();
+                
+                s.storing(name,subject, phone, roll);
+                s.display();
+                break;
+                
             case 2:
-                    String sql = "SELECT * FROM student_info";
-                    ResultSet re =smt.executeQuery(sql);
-                    while (re.next()) {                    
-                        
-                        int id = re.getInt("roll");
-                        String name = re.getString("name");
-                        String subject = re.getString("subject");
-                        int phone = re.getInt("phone");
+                sc.nextLine();
+                System.out.print("Enter compayname-");
+                company=sc.nextLine();
+                
+                System.out.print("Enter clientname-");
+                client=sc.nextLine();
+                
+                c.companydata(company, client);
+                c.display();
+                break;    
+        }        
+    }while(opt!=3);
+  }     
+}
 
-                        System.out.println(id+"   "+name+"    "+subject+"    "+phone);
-                    }
-                    break;
+class Student{
+    private  final String dbClassName = "com.mysql.jdbc.Driver";
+    private  final String CONNECTION ="jdbc:mysql://127.0.0.1/student";
+    private Connection c,c1 =null;
+    private Statement smt,smt1 =null;
+     
+    public void storing(String var1, String var2 ,int var3, int var4){   //name,sub,phone,roll
+    
+        try {
+            Class.forName(dbClassName);
+            Properties p = new Properties();
+            p.put("user","root");
+            p.put("password","");
+            c = DriverManager.getConnection(CONNECTION,p);   
+        
+            smt = c.createStatement();
+            smt.executeUpdate("INSERT INTO `student_info`(roll , name , subject , phone)"+
+              "VALUES('"+var4+"','"+var1+"','"+var2+"','"+var3+"')");
+            System.out.println("It works !");
+        }
+        
+        catch (ClassNotFoundException e) {
+            System.out.println("Driver not istalled"+ e);
+        }
+        catch(SQLException e){
+            System.out.println("Not connected to mysql"+ e);
+        }
+        catch(Exception e){
+            System.out.println("some other exection");
+        }
+        finally{
+            try {
+                c.close();
             }
-  
-    }while(choice!=3);
-    c.close();
- }
+            catch(SQLException e){
+                 System.out.println("Not connected to mysql"+ e);
+            }
+        }
+    
+    
+    }
+    
+    public void display(){
+      
+        try {
+            Class.forName(dbClassName);
+            Properties p = new Properties();
+            p.put("user","root");
+            p.put("password","");
+            c1 = DriverManager.getConnection(CONNECTION,p);  
+            smt1 = c1.createStatement();
+            String sql = "SELECT * FROM student_info";
+            ResultSet re =smt1.executeQuery(sql);
+            while (re.next()) {                    
+
+                int id = re.getInt("roll");
+                String name = re.getString("name");
+                String subject = re.getString("subject");
+                int phone = re.getInt("phone");
+
+                System.out.println(id+"   "+name+"    "+subject+"    "+phone);
+            }
+        }
+         catch (ClassNotFoundException e) {
+            System.out.println("Driver not istalled"+ e);
+        }
+        catch(SQLException e){
+            System.out.println("Not connected to mysql"+ e);
+        }
+        catch(Exception e){
+            System.out.println("some other exection"+e);
+        }
+        finally{
+            try {
+                c1.close();
+
+            }
+            catch(SQLException e){
+                 System.out.println("Not  to mysql"+ e);
+            }
+        }
+    }
 }
 
 
 
+class Company{
+    private  final String dbClassName = "com.mysql.jdbc.Driver";
+    private  final String CONNECTION ="jdbc:mysql://127.0.0.1/student";
+    private Connection c,c1 =null;
+    private Statement smt,smt1 =null;
+     
+    public void companydata(String var1, String var2 ){   //CompanyName,ClientName
+    
+        try {
+            Class.forName(dbClassName);
+            Properties p = new Properties();
+            p.put("user","root");
+            p.put("password","");
+            c = DriverManager.getConnection(CONNECTION,p);   
+        
+            smt = c.createStatement();
+            smt.executeUpdate("INSERT INTO `company_info`(CompanyName,ClientName)"+
+              "VALUES('"+var1+"','"+var2+"')");
+            System.out.println("It works !");
+        }
+        
+        catch (ClassNotFoundException e) {
+            System.out.println("Driver not istalled"+ e);
+        }
+        catch(SQLException e){
+            System.out.println("Not connected to mysql"+ e);
+        }
+        catch(Exception e){
+            System.out.println("some other exection");
+        }
+        finally{
+            try {
+                c.close();
+            }
+            catch(SQLException e){
+                 System.out.println("Not connected to mysql"+ e);
+            }
+        }
+    
+    
+    }
+    
+    public void display(){
+      
+        try {
+            Class.forName(dbClassName);
+            Properties p = new Properties();
+            p.put("user","root");
+            p.put("password","");
+            c1 = DriverManager.getConnection(CONNECTION,p);  
+            smt1 = c1.createStatement();
+            String sql = "SELECT * FROM company_info";
+            ResultSet re =smt1.executeQuery(sql);
+            while (re.next()) {                    
 
+                String company = re.getString("CompanyName");
+                String client = re.getString("ClientName");
+                
+                System.out.println(company+"    "+client);
+            }
+        }
+         catch (ClassNotFoundException e) {
+            System.out.println("Driver not istalled"+ e);
+        }
+        catch(SQLException e){
+            System.out.println("Not connected to mysql"+ e);
+        }
+        catch(Exception e){
+            System.out.println("some other exection"+e);
+        }
+        finally{
+            try {
+                c1.close();
 
-
-
-
-
-
-
-
-
-
-
-
-
+            }
+            catch(SQLException e){
+                 System.out.println("Not  to mysql"+ e);
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -4,6 +4,6 @@ Simple program in java which takes store the information in database,and then re
 
 Here the data which is passed from console is stored in database.
 
-![](result.png)
+![](company.png)
 
-Here the data is retrieve from database and display on console.
+Here the data of the another table i.e. company table.
